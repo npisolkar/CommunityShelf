ccf3e4e40d6695c37c8fcb05b41f57f6fc7b68fc
npisolkar <90062644+npisolkar@users.noreply.github.com>
2023-11-05 23:39:57 -0500

    stage 1 submit

---
+++ mysql_text.py
@@ -0,0 +1,29 @@
+import mysql.connector
+
+# Replace these with your MySQL server information
+db_config = {
+    "host": "localhost",
+    "user": "root",
+    "password": "Co!!ege3oard2",
+    "database": "community_shelf",
+}
+
+try:
+    connection = mysql.connector.connect(**db_config)
+    if connection.is_connected():
+        print("Connected to MySQL")
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
+#finally:
+
+cursor = connection.cursor()
+
+
+cursor.execute("""INSERT into users (name, email, phoneNumber, state, county, address, zip, password) values ("James B", "jamesb@purdue.edu", "3025889267", "PA", "Chester", "881 Brintons Bridge Road", "19382", "JamesPassword" )""")
+connection.commit()
+
+connection.close()
+
+
+
+
